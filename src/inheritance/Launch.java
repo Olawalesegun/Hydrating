fda968572da4630e24fde2552f664d128084b574
olawalesegun <olawalesegun0@gmail.com>
2025-01-26 01:33:32 +0300

    add:: Is-A --> Inheritance, while Has-A ---> Composition

--- src/inheritance/Launch.java
+++ src/inheritance/Launch.java
@@ -3,10 +3,13 @@ package inheritance;
 public class Launch {
 
     public static void main(String[] args){
-        IPhone iphone7 = new IPhone();
+        IPhone iphone7 = new IPhone();   // HAS--A  Composition
         iphone7.call();
         iphone7.browse_phone();
         iphone7.end_call();
 
+        Phone iphone14 = new IPhone();
+        iphone14.call();
+        iphone14.end_call();
     }
 }
