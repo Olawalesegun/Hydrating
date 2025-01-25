990c15f04fa57c60749676da6f076364fd5495a6
olawalesegun <olawalesegun0@gmail.com>
2025-01-26 01:37:25 +0300

    add:: Is-A --> Inheritance, while Has-A ---> Composition

--- src/inheritance/IPhone.java
+++ src/inheritance/IPhone.java
@@ -2,6 +2,10 @@ package inheritance;
 
 public class IPhone extends Phone{
 
+    // IS-A means it extended and is of that type
+    // HAS-A means it has that type, elements of field as properties in it
+        // , this is not same as IS_A, this can be used to represent that
+        // elements are present in it.
     public void browse_phone(){
         System.out.println("Browse Phone...");
     }
