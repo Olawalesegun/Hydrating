39ced1e61941bcb8030825ced61fb7580df84a53
olawalesegun <olawalesegun0@gmail.com>
2025-01-27 18:06:04 +0300

    add:: run time polymorphism -- method overriding implemented

--- src/polymorphism/Launch3.java
+++ src/polymorphism/Launch3.java
@@ -21,6 +21,32 @@ class Boy extends Human3{
     public void sleep(){
         System.out.println("Boy sleeps gently on the Floor");
     }
+
+    public void play_football(){
+        System.out.println("Boy is playing football with a friend");
+    }
+
+    public String toString(){
+        return "Boy";
+    }
+}
+
+class Girl extends Human3{
+    public void speak(){
+        System.out.println("Speaking stylishly as a girl!!");
+    }
+
+    public void sleep(){
+        System.out.println("Sleeping gently as a virtuous girl");
+    }
+
+    public void plait_hair(){
+        System.out.println("Gir plaits hair");
+    }
+
+    public String toString(){
+        return "Girl";
+    }
 }
 
 class ClassRoom{
@@ -28,6 +54,13 @@ class ClassRoom{
     public Human3 hostHuman(Human3 hum){
         hum.sleep();
         hum.speak();
+        if(hum instanceof Boy){
+            ((Boy)hum).play_football();
+        }
+        else if(hum instanceof Girl){
+            ((Girl) hum).plait_hair();
+        }
+
         return hum;
     }
 
@@ -35,7 +68,14 @@ class ClassRoom{
 public class Launch3 {
 
     public static void main(String[] args) {
+        ClassRoom cr = new ClassRoom();
+        Boy boy = new Boy();
+        Girl girl = new Girl();
 
+        cr.hostHuman(boy);
+        System.out.println("-----------------------------------");
+        System.out.println("-----------------------------------");
+        cr.hostHuman(girl);
     }
 
 }
