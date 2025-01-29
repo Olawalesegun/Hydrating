de681d0d6db58f84112e1386e5f846c41787c308
olawalesegun <olawalesegun0@gmail.com>
2025-01-29 13:46:09 +0300

    add: polymorphism implement while using super() and this()

---
+++ src/polymorphism/Launch4.java
@@ -0,0 +1,44 @@
+package polymorphism;
+
+class Vehicle{
+    public void move_gear(){
+        System.out.println("Vehicle Gear is moved");
+    }
+
+    public void change_gear_box(){
+        System.out.println("Vehicle gear is being changed");
+    }
+}
+
+class Trailer extends Vehicle{
+    public Trailer(){
+        this("Bardock");
+        System.out.println("Trailer is a Vehicle, and it's been executed");
+    }
+
+    public Trailer(String trailerName){
+        super();
+        System.out.println(trailerName + " fully activated");
+    }
+
+    @Override
+    public void move_gear(){
+        System.out.println("Trailer Gear is moved");
+    }
+
+    @Override
+    public void change_gear_box(){
+        System.out.println("Trailer Gears are being changed.");
+    }
+    @Override
+    public String toString(){
+        return "Trailer";
+    }
+}
+
+public class Launch4 {
+    public static void main(String[] args) {
+        Trailer trail = new Trailer();
+        System.out.println(trail);
+    }
+}
